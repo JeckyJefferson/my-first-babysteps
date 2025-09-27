9105ae5d4ed53466141996bf193eb8ca5ef5fca8
Jecky <agentstocks88@gmail.com>
2025-09-27 21:22:40 +0700

    My first Python code with SSH

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 """
+<<<<<<< HEAD
 Ini adalah project demo pertama dengan Python
+=======
+Ini adalah projek demo pertama saya dengan Python
+>>>>>>> eba47b0 (My first Python code)
 """
 print("Hello World")
 print("My name is Jecky")
